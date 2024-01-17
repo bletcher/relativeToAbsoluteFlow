c2bfabf4f549c187142d9ef809b35b21a0520c3c
Ben Letcher <bletcher@usgs.gov>
2024-01-17 15:30:56 -0500

    first

---
+++ buildBook.R
@@ -0,0 +1,11 @@
+library(quarto)
+
+
+library(getPrepareWBData)
+library(getWBData)
+
+# may need to do this if packages don't load
+# install.packages("xxxx", dependencies=TRUE, repos='http://cran.rstudio.com/')
+
+
+quarto::quarto_render(output_format = "html")
